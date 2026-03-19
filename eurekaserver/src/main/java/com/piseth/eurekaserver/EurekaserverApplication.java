bb98de79b96e02cc1e954127018121d629fc14a8
PisethMao <pisethmao2002@gmail.com>
2026-03-19 11:08:29 +0700

    configure eureka

---
+++ eurekaserver/src/main/java/com/piseth/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.piseth.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+
+}
